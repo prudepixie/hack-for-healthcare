26fe5d186bf29acd51e4449270fa926a8efd5f1a
Wendy Hsu <wndyhsu@gmail.com>
2016-08-27 14:14:37 -0700

    add sign in / sign out routes to welcome index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  # def after_sign_in_path_for(resource)
-  #   stored_location_for(resource) || current_doctor.sign_in_path
-  # end
+  def after_sign_in_path_for(resource)
+    patients_path
+  end
 end
